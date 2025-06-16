e1edda3a1584757510e411d6e8374023ee39a17e
ClementLuong <phantom0311@hotmail.com>
2025-06-16 18:54:52 +1000

    UI update
    - Added pinch and zoom in gallery.
    - Added drag to toggle multi-select in download mode for Camera SD mode and intervalometer save mode.
    - Fixed gallery/main activity closing/disconnecting on screen rotation.

--- app/src/main/java/com/example/sonyrx10m3remote/GalleryActivity.kt
+++ app/src/main/java/com/example/sonyrx10m3remote/GalleryActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.ViewModelProvider
 import com.example.sonyrx10m3remote.camera.CameraControllerProvider
 import com.example.sonyrx10m3remote.gallery.GalleryScreen
 import com.example.sonyrx10m3remote.gallery.GalleryViewModel
@@ -14,21 +14,24 @@ import com.example.sonyrx10m3remote.ui.theme.RX10M3RemoteTheme
 import kotlinx.coroutines.launch
 
 class GalleryActivity : ComponentActivity() {
+    private lateinit var viewModel: GalleryViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val mediaManager = MediaManagerProvider.instance
         val cameraController = CameraControllerProvider.instance
-
         val cameraId = intent.getStringExtra("camera_id")
+
+        val factory = GalleryViewModelFactory(applicationContext, mediaManager, cameraController, cameraId)
+        viewModel = ViewModelProvider(this, factory)[GalleryViewModel::class.java]
+
+        lifecycleScope.launch {
+            viewModel.onGalleryOpened()
+        }
 
         setContent {
             RX10M3RemoteTheme {
-                val viewModel: GalleryViewModel = viewModel(
-                    factory = GalleryViewModelFactory(applicationContext, mediaManager, cameraController, cameraId)
-                )
-
                 GalleryScreen(viewModel = viewModel,
                     cameraController = CameraControllerProvider.instance,
                     mediaManager = MediaManagerProvider.instance,
@@ -36,6 +39,13 @@ class GalleryActivity : ComponentActivity() {
                     onDownloadSelected = { viewModel.downloadChosenImages() }
                 )
             }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (isFinishing) {
+            viewModel.onGalleryClosed()
         }
     }
 }
